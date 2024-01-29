352404ec0e8493e7d15a308f8747d65c909da12e
jaeeun <rosa2070@naver.com>
2024-01-29 13:49:47 +0900

    01/29

---
+++ src/sec03/chap06/Ex01.java
@@ -0,0 +1,15 @@
+package sec03.chap06;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        int int1 = "".length();
+        int int2 = "헬로".length();
+        int int3 = "Hello".length();
+        int int4 = "김수한무 거북이와 두루미".length();
+
+        int int5 = "안녕하세요".length();
+        String str = "Hello World";
+        int int6 = str.length();
+    }
+}
