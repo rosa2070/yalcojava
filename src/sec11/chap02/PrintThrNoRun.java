f87370366bc5f58884aba186dfef7c3e41069405
jaeeun <rosa2070@naver.com>
2024-03-12 15:08:40 +0900

    03/12

---
+++ src/sec11/chap02/PrintThrNoRun.java
@@ -0,0 +1,19 @@
+package sec11.chap02;
+
+public class PrintThrNoRun implements Runnable {
+    int no;
+
+    public PrintThrNoRun(int no) {
+        this.no = no;
+    }
+
+    @Override
+    public void run() {
+        for (int i=0; i<20; i++) {
+            System.out.print(no);
+
+            // 시간지연
+            for(int j=0; j<Integer.MAX_VALUE; j++) {}
+        }
+    }
+}
---
+++ src/sec11/chap05/ex02/ManagerRun.java
@@ -0,0 +1,16 @@
+package sec11.chap05.ex02;
+
+public class ManagerRun implements Runnable {
+    CoffeeMachine coffeeMachine;
+
+    public ManagerRun(CoffeeMachine coffeeMachine) {
+        this.coffeeMachine = coffeeMachine;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            coffeeMachine.fill();
+        }
+    }
+}
